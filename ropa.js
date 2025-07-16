d5024e5b275ec37417ea5ab62dfd18d2d26d202d
lautaro <lautaroluceroschwindt@gmail.com>
2025-07-16 11:01:43 -0300

    primer commit

---
+++ ropa.js
@@ -0,0 +1,199 @@
+const abrigos = ["prendas de sol/abrigos/campera beige abrigada (me encanta).jpg", "prendas de sol/abrigos/campera marron tipo pañuelo.jpg", "prendas de sol/abrigos/campera negra con botones.jpg",
+    "prendas de sol/abrigos/campera negra.jpg", "prendas de sol/abrigos/chaqueta negra corta con capucha.jpg", "prendas de sol/abrigos/chaqueta negra corta.jpg", "prendas de sol/abrigos/saco beige.jpg"
+];
+
+const buzos = ["prendas de sol/buzos/buzo adidas verde.jpg", "prendas de sol/buzos/buzo arcade violeta.jpg", "prendas de sol/buzos/buzo blanco botones al costado.jpg", "prendas de sol/buzos/buzo blanco con cierre y capucha.jpg",
+"prendas de sol/buzos/buzo blanco con negro en las mangas.jpg", "prendas de sol/buzos/buzo blanco con rayas azules.jpg", "prendas de sol/buzos/buzo celeste.jpg", "prendas de sol/buzos/buzo negro con gatos.jpg",
+"prendas de sol/buzos/buzo negro con rayas blancas.jpg", "prendas de sol/buzos/buzo verde hecho top (se nota sol).jpg", "prendas de sol/buzos/campera de river.jpg",
+"prendas de sol/buzos/campera gris con capucha.jpg", "prendas de sol/buzos/chaleco blanco fino con mangas (no se).jpg", "prendas de sol/buzos/sueter azul con equiz blancas.jpg",
+"prendas de sol/buzos/sueter azul francia.jpg", "prendas de sol/buzos/sueter azul marino botones.jpg", "prendas de sol/buzos/sueter azul marino de lana gruesa.jpg",
+"prendas de sol/buzos/sueter azul marino.jpg", "prendas de sol/buzos/sueter blanco con bolsillos (no lo puedo creer).jpg", "prendas de sol/buzos/sueter blanco de lana fina.jpg",
+"prendas de sol/buzos/sueter blanco de lana.jpg", "prendas de sol/buzos/sueter colores de jardin con botones.jpg", "prendas de sol/buzos/sueter con botones negro de lana.jpg",
+"prendas de sol/buzos/sueter cremita 2.jpg", "prendas de sol/buzos/sueter cremita de lana.jpg", "prendas de sol/buzos/sueter cremita.jpg", "prendas de sol/buzos/sueter gris con 12.jpg", "prendas de sol/buzos/sueter gris cremita.jpg",
+"prendas de sol/buzos/sueter gris detalles de costura.jpg", "prendas de sol/buzos/sueter rayas negras y grises.jpg", "prendas de sol/buzos/sueter rojo cuello de tortuga.jpg",
+"prendas de sol/buzos/top abrigo marron con botones.jpg", "prendas de sol/buzos/top azul 41 street.jpg", "prendas de sol/buzos/top blanco con capucha y cordones.jpg",
+"prendas de sol/buzos/top blanco con cierre hawai.jpg", "prendas de sol/buzos/top blanco con detalles de bordado.jpg", "prendas de sol/buzos/top blanco de lana.jpg", 
+"prendas de sol/buzos/top con recuadros grises y negros.jpg", "prendas de sol/buzos/top de jean.jpg", "prendas de sol/buzos/top negro de lana con botones.jpg", "prendas de sol/buzos/top negro de lana nudo adelante.jpg",
+"prendas de sol/buzos/top negro lana.jpg", "prendas de sol/buzos/top rosa abrigo.jpg", "prendas de sol/buzos/top rosa chillon agujeritos.jpg", "prendas de sol/buzos/top verde de lana.jpg", "prendas de sol/buzos/top violeta de lana.jpg"];
+
+const remeras = ["prendas de sol/remeras/chomba gris varios botones.jpg", "prendas de sol/remeras/remera amarilla amour.jpg", 
+    "prendas de sol/remeras/remera amarilla detalle de corset.jpg", "prendas de sol/remeras/remera azul 2.jpg", "prendas de sol/remeras/remera azul.jpg", "prendas de sol/remeras/remera blanca con mangas marrones.jpg", 
+    "prendas de sol/remeras/remera blanca con rayas dentro.jpg", "prendas de sol/remeras/remera blanca con rosas.jpg", "prendas de sol/remeras/remera blanca.jpg", "prendas de sol/remeras/remera de rayas de colores.jpg", 
+    "prendas de sol/remeras/remera dorada rayas.jpg", "prendas de sol/remeras/remera fina cremita con anillos.jpg", "prendas de sol/remeras/remera gris y negra mangas cortas.jpg", 
+    "prendas de sol/remeras/remera roja nudo atras.jpg", "prendas de sol/remeras/remera verde claro floreada.jpg", "prendas de sol/remeras/remera verde diseno tipo hojas.jpg", 
+    "prendas de sol/remeras/top amarillo love.jpg", "prendas de sol/remeras/top blanco rayas.jpg", "prendas de sol/remeras/top blanco.jpg", "prendas de sol/remeras/top gris con capucha.jpg", "prendas de sol/remeras/top gris cuello abierto.jpg", 
+    "prendas de sol/remeras/top rojo claro.jpg", "prendas de sol/remeras/top rosa cremita.jpg"];
+
+const pantalones = ["prendas de sol/pantalones/jean azul acampanado.jpg", "prendas de sol/pantalones/jean azul de tiro recto.jpg", "prendas de sol/pantalones/jean azul muy roto.jpg", 
+"prendas de sol/pantalones/jean azul roto.jpg", "prendas de sol/pantalones/jean azul.jpg", "prendas de sol/pantalones/jean gris claro ajustado y acampanado.jpg", "prendas de sol/pantalones/jean gris oscuro.jpg", "prendas de sol/pantalones/jean negro acampanado.jpg", 
+"prendas de sol/pantalones/jean negro acartonado.jpg", "prendas de sol/pantalones/jean negro ajustado y acampanado.jpg", "prendas de sol/pantalones/jean negro sin detalles.jpg", "prendas de sol/pantalones/jean negro.jpg", 
+"prendas de sol/pantalones/pantalon gris.jpg", "prendas de sol/pantalones/pantalon marron oscuro (ponele).jpg", "prendas de sol/pantalones/pantalon rosa.jpg"]
+
+const vestidos = ["prendas de sol/vestido prueba.jpg"]
+/*document.getElementById("random-btn").addEventListener("click", () => {
+ console.log(pantalones);
+});*/
+
+//const dress = document.getElementById("dress");
+const coats = document.getElementById("coats");
+const jackets = document.getElementById("jackets");
+const shirts = document.getElementById("shirts");
+const pants = document.getElementById("pants");
+
+//const dressLength = vestidos.length;
+const coatsLength = abrigos.length;
+const jacketsLength = buzos.length;
+const shirtsLength = remeras.length;
+const pantsLength = pantalones.length;
+
+//let arribaAlternativo = 0;
+let muyArriba = 0;
+let arriba = 0;
+let medio = 0;
+let abajo = 0;
+
+/*function eleccion(){
+  const vestidosOcultos = document.getElementById("vestidosOcultos");
+const remerasOcultas = document.getElementById("remerasOcultas");
+const pantalonesOcultos = document.getElementById("pantalonesOcultos");
+
+  if(vestidosOcultos.style.display === "none"){
+    vestidosOcultos.style.display = "block";
+    remerasOcultas.style.display = "none";
+    pantalonesOcultos.style.display = "none";
+  } else {
+    vestidosOcultos.style.display = "none";
+    remerasOcultas.style.display = "block";
+    pantalonesOcultos.style.display = "block";
+  }
+}*/
+
+function eleccionBuzo(){
+  const buzosOcultos = document.getElementById("buzosOcultos");
+  if(buzosOcultos.style.display === "none"){
+    buzosOcultos.style.display = "block";
+  } else {
+    buzosOcultos.style.display = "none";
+    abrigosOcultos.style.display = "none";
+  }
+}
+
+function eleccionAbrigo(){
+  const abrigosOcultos = document.getElementById("abrigosOcultos");
+  if(abrigosOcultos.style.display === "none"){
+    buzosOcultos.style.display = "block";
+    abrigosOcultos.style.display = "block";
+  } else {
+    abrigosOcultos.style.display = "none";
+  }
+}
+
+/*function vestidosNext(){
+  arribaAlternativo = (arribaAlternativo + 1) % dressLength;
+  dress.src = vestidos[arribaAlternativo];
+}
+
+function vestidosPrev(){
+  arribaAlternativo = (arribaAlternativo - 1 + dressLength) % dressLength;
+  dress.src = vestidos[arribaAlternativo];
+}*/
+
+
+function abrigosNext(){
+  muyArriba = (muyArriba + 1) % coatsLength;
+  coats.src = abrigos[muyArriba];
+}
+
+function abrigosPrev(){
+  muyArriba = (muyArriba - 1 + coatsLength) % coatsLength;
+  coats.src = abrigos[muyArriba];
+}
+
+function buzosNext(){
+  arriba = (arriba + 1) % jacketsLength;
+  jackets.src = buzos[arriba];
+}
+
+function buzosPrev(){
+  arriba = (arriba - 1 + jacketsLength) % jacketsLength;
+  jackets.src = buzos[arriba];
+}
+
+function remerasNext(){
+  medio = (medio + 1) % shirtsLength;
+  shirts.src = remeras[medio];
+}
+
+function remerasPrev(){
+  medio = (medio - 1 + shirtsLength) % shirtsLength;
+  shirts.src = remeras[medio];
+}
+
+function pantalonesNext(){
+  abajo = (abajo + 1) % pantsLength;
+  pants.src = pantalones[abajo];
+}
+
+function pantalonesPrev(){
+  abajo = (abajo - 1 + pantsLength) % pantsLength;
+  pants.src = pantalones[abajo];
+}
+
+function aleatorio(){
+
+   /*arribaAlternativo = Math.floor(Math.random() * vestidos.length);
+  dress.src = vestidos[arribaAlternativo];*/
+
+  muyArriba = Math.floor(Math.random() * abrigos.length);
+  coats.src = abrigos[muyArriba];
+
+  arriba = Math.floor(Math.random() * buzos.length);
+  jackets.src = buzos[arriba];
+
+  medio = Math.floor(Math.random() * remeras.length);
+  shirts.src = remeras[medio];
+
+  abajo = Math.floor(Math.random() * pantalones.length);
+  pants.src = pantalones[abajo];
+}
+
+function giraGira(){
+  const girasol = document.getElementById("girasol");
+  const girasol2 = document.getElementById("girasol2");
+  const girasol3 = document.getElementById("girasol3");
+  const girasol4 = document.getElementById("girasol4");
+  const girasol5 = document.getElementById("girasol5");
+  const girasol6 = document.getElementById("girasol6");
+
+  if(!girasol.classList.contains("visible")){
+  girasol.classList.add("visible");
+  girasol2.classList.add("visible");
+  girasol3.classList.add("visible");
+  girasol4.classList.add("visible");
+  } else{
+    girasol.classList.remove("visible");
+    girasol2.classList.remove("visible");
+    girasol3.classList.remove("visible");
+    girasol4.classList.remove("visible");
+    girasol5.classList.remove("visible");
+    girasol6.classList.remove("visible");
+  }
+}
+
+function giraGira2(){
+  const girasol5 = document.getElementById("girasol5");
+  const girasol6 = document.getElementById("girasol6");
+
+  if(!girasol5.classList.contains("visible")){
+  girasol5.classList.add("visible");
+  girasol6.classList.add("visible");
+  } else{
+    girasol5.classList.remove("visible");
+    girasol6.classList.remove("visible");
+  }
+}
+
+ /*if (window.matchMedia("(max-width: 420px)").matches) {
+       document.getElementById("contenedorGirasoles").classList.remove("col-sm-4");
+       document.getElementById("contenedorGirasoles").classList.remove("col-sm-4");
+     }*/
